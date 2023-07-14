2417759d709bad9f61dececa53a07ba693602229
Nishal Nepal <nepalnishal4@gmail.com>
2023-07-14 08:49:36 -0400

    Add data reader to interpret data and produce a dictionary

--- random_data_gen_consumer.py
+++ random_data_gen_consumer.py
@@ -1,6 +1,10 @@
 from confluent_kafka import Consumer
 from kafka_config.config import config
+import json
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
+import sys
 
 MODE = os.getenv("DEV")
 
@@ -17,20 +21,47 @@ def assignment_callback(consumer, partitions):
             print(f"Assigned to {partition.topic}, partition {partition.partition}")
 
 
+def procure_data(event) -> dict:
+    topic = event.topic()
+    value = json.loads(event.value().decode("utf-8"))
+    final_dict = {}
+    final_dict[topic] = value
+    return final_dict
+
+
+def load_data(data=dict, host=None, username=None, password=None, database=None):
+    if host is None and username is None and password is None:
+        i, j = (list(data.keys())[0], list(data.values())[0])
+        print(i, "\n", j)
+        return
+    if None in [host, username, password]:
+        if host is None:
+            print("Please provide a host")
+        if username is None:
+            print("Please provide a username")
+        if password is None:
+            print("Please provide a password")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     set_configs()
     consumer = Consumer(config)
-    consumer.subscribe(["hello_topic"], on_assign=assignment_callback)
+    consumer.subscribe(
+        ["main", "login", "location", "identification", "image"],
+        on_assign=assignment_callback,
+    )
+
     try:
         while True:
             event = consumer.poll(1.0)
             if event is None:
                 continue
             else:
-                val = event.value().decode("utf-8")
-                key = event.key()
+                data_dict = procure_data(event)
+                load_data(data_dict, "localhost")
                 partition = event.partition()
-                print(f"Received {key=}{val=} from partition {partition}")
+                print(f"Received {data_dict=} from partition {partition}")
                 consumer.commit(event)
     except KeyboardInterrupt:
         print("Interrupted by user.")
--- random_data_gen_producer.py
+++ random_data_gen_producer.py
@@ -12,7 +12,7 @@ def extraction(val: dict, key1: str, key2: str = None) -> dict:
     if not key2:
         return val[key1]
     else:
-        return val[key1][key2] if isinstance(val[key1][key2], dict) else {}
+        return val[key1][key2] if val[key1][key2] else {}
 
 
 def callback(err, event):
@@ -23,21 +23,20 @@ def callback(err, event):
     else:
         val = event.value().decode("utf8")
         if MODE:
-            print(f"{val}, sent to partition {event.partition()}.")
+            print(
+                f"Topic: {event.topic()}\n Value: {val}, sent to partition {event.partition()}."
+            )
 
 
-def export_data(producer, key, value):
-    producer.produce("hello_topic", value, key, on_delivery=callback)
+def export_data(producer, key, value, topic):
+    producer.produce(topic, value, key, on_delivery=callback)
 
 
-def production_loop(random_person_data, results):
-    final_results = {}
-
-    for i in results:
-        if not isinstance(random_person_data["results"][0][i], dict):
-            final_results[i] = random_person_data["results"][0][i]
-
-    return final_results
+def production_loop(producer, final_results):
+    uuid = final_results["main"]["uuid"]
+    for i, j in final_results.items():
+        j = json.dumps(j, indent=4)
+        export_data(producer, uuid, j, i)
 
 
 def main_table(results):
@@ -81,8 +80,8 @@ def location_table(results):
         extraction(results, "location", "coordinates")["latitude"],
         extraction(results, "location", "coordinates")["longitude"],
     )
-    street_name = extraction(results, "street", "name")
-    street_number = extraction(results, "street", "number")
+    street_name = extraction(results, "location", "street")["name"]
+    street_number = extraction(results, "location", "street")["number"]
     city = extraction(results, "location")["city"]
     state = extraction(results, "location")["state"]
     country = extraction(results, "location")["country"]
@@ -177,6 +176,18 @@ def image_table(results):
     return image
 
 
+def assemble_data(results) -> dict:
+    final = {}
+
+    final["main"] = main_table(results)
+    final["login"] = login_table(results)
+    final["location"] = location_table(results)
+    final["identification"] = identification_table(results)
+    final["image"] = image_table(results)
+
+    return final
+
+
 def produce_data(time_seconds=0.1):
     # Producer Stuff
     producer = Producer(config)
@@ -191,16 +202,14 @@ def produce_data(time_seconds=0.1):
         api = requests.get(api_link)
         random_person_data = json.loads(api.text)
         results = random_person_data["results"][0]
-        key = extraction(results, "login")["uuid"]
 
-        final_results = production_loop(random_person_data, results)
+        final_results = assemble_data(results)
 
-        final_results = json.dumps(final_results, indent=4)
-        export_data(producer, key, final_results)
+        production_loop(producer, final_results)
 
         time_from_start = time.time() - time_start
     producer.flush()
 
 
 if __name__ == "__main__":
-    produce_data(1)
+    produce_data()
