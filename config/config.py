5af639a5c6d69ffd1953856a245bbb911536bc6f
Nishal Nepal <nepalnishal4@gmail.com>
2023-10-20 16:18:06 -0400

    Add confluent cloud images

---
+++ config/config.py
@@ -0,0 +1,7 @@
+config = {
+    "bootstrap.servers": "pkc-ymrq7.us-east-2.aws.confluent.cloud:9092",
+    "security.protocol": "SASL_SSL",
+    "sasl.mechanisms": "PLAIN",
+    "sasl.username": "NM7XMBYOWARR26TI",
+    "sasl.password": "aecFH4K+K4UFSHpik+ACnDYJaqlJbpWrcZdkyExb1zgBtksXEE/0uin9VALSkNeg",
+}
